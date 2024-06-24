3dc216c712e59471173561a7fe573176fdfd0de4
Abdullah Bin Omar Chowdhury <90836384+Abdullah00001@users.noreply.github.com>
2024-06-24 21:40:42 +0600

    Update settings.py

--- mamarbank/settings.py
+++ mamarbank/settings.py
@@ -97,8 +97,8 @@ WSGI_APPLICATION = "mamarbank.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        default='postgresql://mamar_bank_2bs2_user:d5eN5W7iGLYGIExcbaDTTbLzdGbgmMEF@dpg-cpsp5to8fa8c739c5fu0-a.oregon-postgres.render.com/mamar_bank_2bs2',
+        
     )
 }
 
